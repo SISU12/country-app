9f1892839e499113cf291b9c75ff9b392fcc5a01
vicky <victormushimire305@gmail.com>
2023-08-09 15:35:26 +0300

    modified files

--- script.js
+++ script.js
@@ -60,6 +60,17 @@ searchBtn.addEventListener("click", () => {
       
       
        `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+      
+      
     });
-  document.querySelector(".error").style.display = "none";
+
+    
+    
 });
